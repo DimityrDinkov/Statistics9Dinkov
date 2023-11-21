c949e975719348ca1390c6b556db254d19f21cc0
pc1-19 <pc1-19@DESKTOP-J8K9N1F>
2023-11-21 15:40:29 +0200

    Add project files.

---
+++ Statistics9Dinkov/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Statistics9Dinkov
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAvr = new System.Windows.Forms.Button();
+            this.btnMode = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lblAvr = new System.Windows.Forms.Label();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAvr
+            // 
+            this.btnAvr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnAvr.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAvr.Location = new System.Drawing.Point(12, 91);
+            this.btnAvr.Name = "btnAvr";
+            this.btnAvr.Size = new System.Drawing.Size(135, 68);
+            this.btnAvr.TabIndex = 0;
+            this.btnAvr.Text = "Average";
+            this.btnAvr.UseVisualStyleBackColor = false;
+            this.btnAvr.Click += new System.EventHandler(this.btnAvr_Click);
+            // 
+            // btnMode
+            // 
+            this.btnMode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMode.Location = new System.Drawing.Point(12, 202);
+            this.btnMode.Name = "btnMode";
+            this.btnMode.Size = new System.Drawing.Size(135, 68);
+            this.btnMode.TabIndex = 1;
+            this.btnMode.Text = "Mode";
+            this.btnMode.UseVisualStyleBackColor = false;
+            this.btnMode.Click += new System.EventHandler(this.btnMode_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtNum.Location = new System.Drawing.Point(241, 38);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(547, 45);
+            this.txtNum.TabIndex = 3;
+            // 
+            // lblAvr
+            // 
+            this.lblAvr.AutoSize = true;
+            this.lblAvr.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblAvr.Location = new System.Drawing.Point(153, 104);
+            this.lblAvr.Name = "lblAvr";
+            this.lblAvr.Size = new System.Drawing.Size(0, 39);
+            this.lblAvr.TabIndex = 4;
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMode.Location = new System.Drawing.Point(153, 215);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(0, 39);
+            this.lblMode.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Numbers:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMode);
+            this.Controls.Add(this.lblAvr);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnMode);
+            this.Controls.Add(this.btnAvr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvr;
+        private System.Windows.Forms.Button btnMode;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lblAvr;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Statistics9Dinkov/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Statistics9Dinkov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAvr_Click(object sender, EventArgs e)
+        {
+            List<double> list = txtNum.Text.Split(' ').Select(l => double.Parse(l)).ToList();
+            var average = list.Average();
+            lblAvr.Text = ("Average is: " + average);
+        }
+
+        private void btnMode_Click(object sender, EventArgs e)
+        {
+            string[] numStrings = txtNum.Text.Split(' ');
+
+            List<int> numbers = new List<int>();
+            foreach (string numString in numStrings)
+            {
+if(int.TryParse(numString, out int num))
+                {
+                    numbers.Add(num);
+                }
+                else
+                {
+                    MessageBox.Show("Error!");
+                    return;
+                }
+            }
+            List<int> modes = FindMode(numbers);
+
+            if(modes.Count > 0)
+            {
+                lblMode.Text = "Mode: " + string.Join(" ", modes);
+            }
+            else
+            {
+                lblMode.Text = "No mode";
+            }
+        }
+        private List<int> FindMode(List<int> numbers)
+        {
+            var frequency = new Dictionary<int, int>();
+
+            foreach (int number in numbers) 
+            {
+                if (frequency.ContainsKey(number))
+                {
+                    frequency[number]++;
+                }
+                else
+                {
+                    frequency[number] = 1;
+                }
+            }
+            int maxFrequency = frequency.Values.Max();
+
+            List<int> modes = frequency.Where(kv => kv.Value == maxFrequency).Select(kv => kv.Key).ToList();
+            return modes;
+        }
+    }
+}
